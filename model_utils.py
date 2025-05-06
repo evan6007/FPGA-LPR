4e5c5a464d753c33816438c81ee969e0d7c664e4
starpig1129 <james911129@gmail.com>
2025-05-06 12:40:48 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   model_utils.py

--- model_utils.py
+++ model_utils.py
@@ -12,6 +12,18 @@ import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset, random_split
 from torch.optim.lr_scheduler import StepLR
 
+import random
+
+
+CHARS = [
+         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K',
+         'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+         'W', 'X', 'Y', 'Z', 'I', 'O', '-'
+         ]
+
+CHARS_DICT = {char:i for i, char in enumerate(CHARS)}
+
 
 class CPMStage_x(nn.Module):
     def __init__(self, in_channels, out_channels):
